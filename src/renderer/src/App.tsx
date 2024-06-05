edf27465aa09a81f444e5de723954a42cc584f67
ImperialBreakCode <eeeda522@gmail.com>
2024-06-05 22:11:05 +0300

    adding font and theming

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
 import { Calendar } from './components/ui/calendar'
+import { ThemeProvider } from './components/ThemeProvider'
 
 function App(): JSX.Element {
 	const [versions] = useState(window.electron.process.versions)
 
 	return (
-		<div className="dark">
-			<Calendar />
-			<ul className="versions">
-				<li>Electron v{versions.electron}</li>
-				<li>Chromium v{versions.chrome}</li>
-				<li>Node v{versions.node}</li>
-			</ul>
-		</div>
+		<ThemeProvider>
+			<div className="bg-background text-foreground">
+				<Calendar mode="default" className="rounded-md border" />
+				<ul className="versions">
+					<li>Electron v{versions.electron}</li>
+					<li>Chromium v{versions.chrome}</li>
+					<li>Node v{versions.node}</li>
+				</ul>
+			</div>
+		</ThemeProvider>
 	)
 }
 
