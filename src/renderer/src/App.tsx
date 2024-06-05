4f20777b619e94751c9decd62df80547952bb8d9
ImperialBreakCode <eeeda522@gmail.com>
2024-06-05 20:00:17 +0300

    updating versions

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import { Calendar } from './components/ui/calendar'
 
 function App(): JSX.Element {
+	const [versions] = useState(window.electron.process.versions)
+
 	return (
 		<div className="dark">
 			<Calendar />
+			<ul className="versions">
+				<li>Electron v{versions.electron}</li>
+				<li>Chromium v{versions.chrome}</li>
+				<li>Node v{versions.node}</li>
+			</ul>
 		</div>
 	)
 }
