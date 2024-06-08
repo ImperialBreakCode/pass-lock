a76a23592128edf77d297d3fbd8e8a704f1595ff
ImperialBreakCode <eeeda522@gmail.com>
2024-06-08 18:14:44 +0300

    automatic system theme change

--- src/renderer/src/components/ThemeProvider.tsx
+++ src/renderer/src/components/ThemeProvider.tsx
@@ -30,21 +30,34 @@ export function ThemeProvider({
 		() => (localStorage.getItem(storageKey) as Theme) || defaultTheme
 	)
 
+	const changeColorMode = (dark: boolean, root: HTMLElement) => {
+		const systemTheme = dark ? 'dark' : 'light'
+		root.classList.remove('light', 'dark')
+		root.classList.add(systemTheme)
+	}
+
 	useEffect(() => {
 		const root = window.document.documentElement
 
 		root.classList.remove('light', 'dark')
 
 		if (theme === 'system') {
-			const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-				? 'dark'
-				: 'light'
+			const matchDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+			changeColorMode(matchDark, root)
 
-			root.classList.add(systemTheme)
+			window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+				changeColorMode(e.matches, root)
+			})
 			return
 		}
 
 		root.classList.add(theme)
+
+		return () => {
+			window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', (e) => {
+				changeColorMode(e.matches, root)
+			})
+		}
 	}, [theme])
 
 	const value = {
