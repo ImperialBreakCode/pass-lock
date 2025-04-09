7b9c7a8bbccfd6e27d2f6b9fa8d7559befd3f3df
ImperialBreakCode <eeeda522@gmail.com>
2025-04-09 19:43:40 +0300

    updating versions

--- src/main/constants/paths.ts
+++ src/main/constants/paths.ts
@@ -1,8 +1,8 @@
-import { app } from 'electron'
-//import path from 'path'
+//import { app } from 'electron'
+import path from 'path'
 
 export const appPaths = {
-	mainDataPath: app.getPath('userData'),
+	mainDataPath: path.join(process.cwd(), '..', 'passlocktestdata'),
 	passwordStorage: 'passLockStorage',
 	keysStorage: 'keys'
 }
