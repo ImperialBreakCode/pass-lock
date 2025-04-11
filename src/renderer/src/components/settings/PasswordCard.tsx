5388ed855a72369663e17e8bd53a8a0b574f3585
ImperialBreakCode <eeeda522@gmail.com>
2025-04-11 13:56:12 +0300

    adding set master password dialog

--- src/renderer/src/components/settings/PasswordCard.tsx
+++ src/renderer/src/components/settings/PasswordCard.tsx
@@ -2,14 +2,20 @@ import { Button } from '../ui/button'
 import { Card, CardDescription, CardHeader, CardTitle } from '../ui/card'
 import { Separator } from '../ui/separator'
 
-function PasswordCard() {
+interface PasswordCardProps {
+	openMasterPasswordDialogue: () => void
+}
+
+function PasswordCard({ openMasterPasswordDialogue }: PasswordCardProps) {
 	return (
 		<Card className="border-border">
 			<CardHeader>
 				<CardTitle className="mb-2">Password Management</CardTitle>
 				<Separator />
 				<CardDescription className="mt-2">
-					<Button variant={'secondary'}>Set Master Password</Button>
+					<Button onClick={() => openMasterPasswordDialogue()} variant={'secondary'}>
+						Set Master Password
+					</Button>
 				</CardDescription>
 			</CardHeader>
 		</Card>
--- src/renderer/src/pages/Settings.tsx
+++ src/renderer/src/pages/Settings.tsx
@@ -1,15 +1,29 @@
 import DataCard from '@/components/settings/DataCard'
 import PasswordCard from '@/components/settings/PasswordCard'
+import SetMasterPasswordDialog from '@/components/settings/SetMasterPasswordDialog'
 import PageHeader from '@/elements/PageHeader'
 import PageWrapper from '@/elements/PageWrapper'
+import SideSheet from '@/elements/SideSheet'
+import { useState } from 'react'
 
 function Settings() {
+	const [masterPasswordOpen, setMasterPasswordOpen] = useState(false)
+
 	return (
 		<PageWrapper>
+			<SideSheet
+				open={masterPasswordOpen}
+				onOpenChange={setMasterPasswordOpen}
+				title="Set Master Password"
+				description={`Once you've set your master password, you'll need to add your first account information for the password to be saved and used.`}
+			>
+				<SetMasterPasswordDialog onSuccessfullSubmit={() => setMasterPasswordOpen(false)} />
+			</SideSheet>
+
 			<PageHeader pageTitle="Settings" />
 			<div className="mt-4">
 				<div className="mb-2">
-					<PasswordCard />
+					<PasswordCard openMasterPasswordDialogue={() => setMasterPasswordOpen(true)} />
 				</div>
 
 				<div>
